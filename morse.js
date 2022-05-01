578ae98f2dc1c14c86c2ca9eccf2ad049e000287
Clint Kingston <ckingston1@woolworths.com.au>
2022-05-01 14:15:13 +1000

    moved constants and event listener to top of file

--- morse.js
+++ morse.js
@@ -1,3 +1,8 @@
+const userInput = document.getElementById('input');
+const morseOutput = document.getElementById('output');
+const encodeButton = document.getElementById('encodeButton');
+encodeButton.addEventListener('click', function () { textOrMorse(userInput.value); });
+
 const morse = {
     A: ".-",
     B: "-...",
@@ -46,10 +51,7 @@ const textOrMorse = (message) => {
     return morse2msg(message);
 };
 
-
 const msg2morse = (message) => {
-    console.log('msg2morse');
-    
     const morseArray = message.toUpperCase().split("").map(char => char == " " ? "_" : char);
     return encode(morseArray);
 };
@@ -59,32 +61,22 @@ const encode = (message) => {
     for (i = 0; i <= message.length; i++) {
         morseCode[i] = morse[message[i]];
     }
-    console.log(morseCode.join(" "));
     return output(morseCode.join(" "));
 };
 
-
-
 const morse2msg = (morse) => {
-    console.log('morse2msg');
     const msgArray = morse.split(" ");
-    console.log(msgArray);
     return decode(msgArray);
 };
-
 
 const decode = (message) => {
     const text = message.map(code => valueKeys.indexOf(code));
     const decodedMessage = text.map(char => keyValues[char] == "_" ? " " : keyValues[char]).join("");
-    console.log("keys", text);
-    console.log("values", decodedMessage);
     return output(decodedMessage);
 };
 
 const output = (encodedMessage) => {
     morseOutput.value = encodedMessage;
 };
-const userInput = document.getElementById('input');
-const morseOutput = document.getElementById('output');
-const encodeButton = document.getElementById('encodeButton');
-encodeButton.addEventListener('click', function () { textOrMorse(userInput.value); });
+
+
