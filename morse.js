4e9c5ceec6520d0bf8dc6e57b091fd7a83c798a4
Clint Kingston <ckingston1@woolworths.com.au>
2022-05-01 16:07:19 +1000

    feature implemented

--- morse.js
+++ morse.js
@@ -31,28 +31,31 @@ const morse = {
     Y: "-.--",
     Z: "--..",
     _: "/",
-    _1:".----",
-    _2:"..---",
-    _3:"...--",
-    _4:"....-",
-    _5:".....",
-    _6:"-....",
-    _7:"--...",
-    _8:"---..",
-    _9:"----.",
-    _0:"-----",
+    _1: ".----",
+    _2: "..---",
+    _3: "...--",
+    _4: "....-",
+    _5: ".....",
+    _6: "-....",
+    _7: "--...",
+    _8: "---..",
+    _9: "----.",
+    _0: "-----",
 };
 
 valueKeys = Object.values(morse);
 keyValues = Object.keys(morse);
 
 const textOrMorse = (message) => {
-    if (message.match(/[a-zA-Z]/)) return msg2morse(message);
+    if (message.match(/[a-zA-Z0-9]/)) return msg2morse(message);
     return morse2msg(message);
 };
 
 const msg2morse = (message) => {
-    const morseArray = message.toUpperCase().split("").map(char => char == " " ? "_" : char);
+    let morseArray = message.toUpperCase().split("").map(char => char == " " ? "_" : char);
+    for (i = 0; i <= 9; i++) {
+        morseArray = morseArray.map(char => char == i ? "_" + i : char);
+    }
     return encode(morseArray);
 };
 
@@ -71,7 +74,19 @@ const morse2msg = (morse) => {
 
 const decode = (message) => {
     const text = message.map(code => valueKeys.indexOf(code));
-    const decodedMessage = text.map(char => keyValues[char] == "_" ? " " : keyValues[char]).join("");
+    const decodedMessage = text.map(char => keyValues[char] == "_" ? " "
+        : keyValues[char] == "_1" ? "1"
+        : keyValues[char] == "_2" ? "2"
+        : keyValues[char] == "_3" ? "3"
+        : keyValues[char] == "_4" ? "4"
+        : keyValues[char] == "_5" ? "5"
+        : keyValues[char] == "_6" ? "6"
+        : keyValues[char] == "_7" ? "7"
+        : keyValues[char] == "_8" ? "8"
+        : keyValues[char] == "_9" ? "9"
+        : keyValues[char] == "_0" ? "0"
+        : keyValues[char]).join("");
+
     return output(decodedMessage);
 };
 
